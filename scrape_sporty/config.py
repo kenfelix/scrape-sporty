b8ee52f54f94d420dc788a38b527334fa76eb91e
Emeka Okafor <kmrapper.kf@gmail.com>
2024-11-01 22:53:48 +0100

    fix: switch to firefox web driver

--- scrape_sporty/config.py
+++ scrape_sporty/config.py
@@ -1,23 +1,23 @@
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
-from selenium.webdriver.edge.options import Options
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
 from contextlib import contextmanager
 
 @contextmanager
-def edge_driver_generator():
-    """Generator that yields a Selenium Edge WebDriver instance."""
+def firefox_driver_generator():
+    """Generator that yields a Selenium Firefox WebDriver instance."""
     options = Options()
     options.add_argument(
         "--headless"
     )  # Run in headless mode if you don't need a visible browser
 
     driver_service = Service(
-        EdgeChromiumDriverManager().install()
+        GeckoDriverManager().install()
     )  # Automatically downloads the GeckoDriver
 
     try:
-        driver = webdriver.Edge(service=driver_service, options=options)
+        driver = webdriver.Firefox(service=driver_service, options=options)
         driver.maximize_window()
         yield driver
     finally:
